715858bf587535533670d847131d23dd5f828b86
Hrushikesh Sakunde <sakundehrushi@gmail.com>
2018-12-04 10:59:46 +0100

    Add company name in search query

--- src/main/java/com/jobportal/demo/repositories/JobRepository.java
+++ src/main/java/com/jobportal/demo/repositories/JobRepository.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "http://localhost:4200")
 public interface JobRepository extends JpaRepository<Job, Long>  {
 
-    @Query("SELECT j FROM Job j WHERE title LIKE %?1% OR content LIKE %?1% OR tasks LIKE %?1% OR profile LIKE %?1% OR location LIKE %?1%")
+    @Query("SELECT j FROM Job j WHERE title LIKE %?1% OR content LIKE %?1% OR tasks LIKE %?1% OR profile LIKE %?1% OR location LIKE %?1% OR companyName LIKE %?1%")
     List<Job> findJobs(String key);
 
 }
